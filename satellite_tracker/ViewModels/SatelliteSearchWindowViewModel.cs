b6ff7f941ebc9f268a730b54945b332870c3dd4c
newtype0096 <newtype0096@naver.com>
2024-10-11 16:59:10 +0900

    set datagrid theme

--- satellite_tracker/ViewModels/SatelliteSearchWindowViewModel.cs
+++ satellite_tracker/ViewModels/SatelliteSearchWindowViewModel.cs
@@ -3,18 +3,31 @@ using CelesTrakLib.Data;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using MvvmDialogs;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace satellite_tracker.ViewModels
 {
     public class SatelliteSearchWindowViewModel : ObservableObject, IModalDialogViewModel
     {
+        public bool? DialogResult { get; private set; }
+
+        private bool _isBusy = false;
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set => SetProperty(ref _isBusy, value);
+        }
+
+        private ObservableCollection<SatelliteCatalog> _satelliteCatalogs;
+        public ObservableCollection<SatelliteCatalog> SatelliteCatalog
+        {
+            get => _satelliteCatalogs;
+            set => SetProperty(ref _satelliteCatalogs, value);
+        }
+
+        public RelayCommand InitDialogCommand { get; }
+
         public SatelliteSearchWindowViewModel()
         {
             InitDialogCommand = new RelayCommand(OnInitDialog);
@@ -22,7 +35,20 @@ namespace satellite_tracker.ViewModels
 
         private void OnInitDialog()
         {
-            LoadSatcat();
+            IsBusy = true;
+
+            var worker = new BackgroundWorker();
+            worker.DoWork +=
+                (sender, e) =>
+                {
+                    LoadSatcat();
+                };
+            worker.RunWorkerCompleted +=
+                (sender, e) =>
+                {
+                    IsBusy = false;
+                };
+            worker.RunWorkerAsync();
         }
 
         private void LoadSatcat()
@@ -32,17 +58,5 @@ namespace satellite_tracker.ViewModels
                 SatelliteCatalog = new ObservableCollection<SatelliteCatalog>(response.SatelliteCatalogs);
             }
         }
-
-        public bool? DialogResult { get; private set; }
-
-        public ObservableCollection<SatelliteCatalog> SatelliteCatalog
-        {
-            get => _satelliteCatalogs;
-            set => SetProperty(ref _satelliteCatalogs, value);
-        }
-
-        public RelayCommand InitDialogCommand { get; }
-
-        private ObservableCollection<SatelliteCatalog> _satelliteCatalogs;
     }
 }
